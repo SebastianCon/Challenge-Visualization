879f59b68587962099a5f915a98a2ac6396f25e6
SebastianCon <sebastiancontrerasdelmoral@gmail.com>
2020-03-31 16:27:32 +0200

    starting from sample project

---
+++ src/communities.ts
@@ -0,0 +1,202 @@
+export const latLongCommunities = [
+  {
+    name: "Niederbayern",
+    long: 13.0208058,
+    lat: 48.7896497
+  },
+  {
+    name: "Oberbayern",
+    long: 11.8094205,
+    lat: 48.0019606
+  },
+  {  
+    name: "Oberfranken",
+    long: 11.0059895,
+    lat: 50.1926498
+  },
+  {
+    name: "Oberpfalz",
+    long: 12.189278,
+    lat: 49.2802811
+  },
+  {
+    name: "Schwaben",
+    long: 10.4190996,
+    lat: 48.1894913
+  },
+  {
+    name: "Unterfranken",
+    long: 10.0232096,
+    lat: 50.1367493
+  },
+  {
+    name: "Freiburg",
+    long: 8,
+    lat: 48
+  },
+  {
+    name: "Karlsruhe",
+    long: 8.5536527,
+    lat: 49.0068901
+  },
+  {
+    name: "Stuttgart",
+    long: 9.1770201,
+    lat: 48.7823181
+  },
+  {
+    name: "Tübingen",
+    long: 9.3576448,
+    lat: 48.0116364
+  },
+  {
+    name: "Mittelfranken",
+    long: 10.6506315,
+    lat: 49.3633273
+  },
+  {
+    name: "Berlin",
+    long: 13.4105301,
+    lat: 52.5243683 
+  },
+  {
+    name: "Brandenburg",
+    long: 12.5316444,
+    lat: 52.4125287
+  },
+  {
+    name: "Bremen",
+    long: 8.8077698,
+    lat: 53.075161
+  },
+  {
+    name: "Hamburg",
+    long: 10.0153399,
+    lat: 53.5753212
+  },
+  {
+    name: "Darmstadt",
+    long: 8.6502705,
+    lat: 49.8716698
+  },
+  {
+    name: "Gießen",
+    long: 8.6784031,
+    lat: 50.5840512
+  },
+  {
+    name: "Kassel",
+    long: 9.5,
+    lat: 50.9166695
+  },
+  {
+    name: "Mecklenburg-Vorpommern",
+    long: 12.4295953,
+    lat: 53.6126505
+  },
+  {
+    name: "Braunschweig",
+    long: 10.5267696,
+    lat: 	52.2688736
+  },
+  {
+    name: "Hannover",
+    long: 9.7320104,
+    lat: 52.3758916
+  },
+  {
+    name: "Luneburg",
+    long: 10.4140902,
+    lat: 53.2509003
+  },
+  {  
+    name: "Weser-Ems",
+    long: 7.903685,
+    lat: 53.014695
+  },
+  {
+    name: "Arnsberg",
+    long: 8.0833302,
+    lat: 51.3833313
+  },
+  {
+    name: "Detmold",
+    long: 8.8731804,
+    lat: 51.938549
+  },
+  {
+    name: "Düsseldorf",
+    long: 6.7761598,
+    lat: 51.2217216
+  },
+  {
+    name: "Köln",
+    long: 6.9602786,
+    lat: 50.837531
+  },
+  {
+    name: "Munster",
+    long: 7.4840147,
+    lat: 51.9500023
+  },
+  {
+    name: "Koblenz",
+    long: 7.5889959,
+    lat: 50.3569429
+  },
+  {
+    name: "Rheinhessen-Pfalz",
+    long: 8.0850303,
+    lat: 49.3935328
+  },
+  {
+    name: "Trier",
+    long: 6.7598698,
+    lat: 49.96231
+  },
+  {
+    name: "Saarland",
+    long: 7.0229607,
+    lat: 49.3964234
+  },
+  {
+    name: "Chemnitz",
+    long: 12.9213697,
+    lat: 50.827845
+  },
+  {
+    name: "Dresden",
+    long: 13.7372621,
+    lat: 51.0504088
+  },
+  {
+    name: "Leipzig",
+    long: 12.3730747,
+    lat: 51.3396955
+  },
+  {
+    name: "Dessau",
+    long: 12.2455502,
+    lat: 51.8386383
+  },
+  {
+    name: "Halle",
+    long: 11.8322906,
+    lat: 51.4727992
+  },
+  {
+    name: "Magdeburg",
+    long: 11.6276237,
+    lat: 52.1205333
+  },
+  {
+    name: "Schleswig-Holstein",
+    long: 9.6961167,
+    lat: 54.2193672
+  },
+  {
+    name: "Thüringen",
+    long: 10.845346,
+    lat: 	51.0109892
+  }
+];
---
+++ src/stats.ts
@@ -0,0 +1,331 @@
+export interface ResultEntry {
+  name: string;
+  value: number;
+}
+
+export const stats15032020: ResultEntry[] = [
+  {
+    name: "Niederbayern",
+    value: 201
+  },
+  {
+    name: "Oberbayern",
+    value: 782
+  },
+  {
+    name: "Oberfranken",
+    value: 325
+  },
+  {
+    name: "Oberpfalz",
+    value: 621
+  },
+  {
+    name: "Schwaben",
+    value: 42
+  },
+  {
+    name: "Unterfranken",
+    value: 23
+  },
+  {
+    name: "Freiburg",
+    value: 85
+  },
+  {
+    name: "Karlsruhe",
+    value: 77
+  },
+  {
+    name: "Stuttgart",
+    value: 384
+  },
+  {
+    name: "Tübingen",
+    value: 144
+  },
+  {
+    name: "Mittelfranken",
+    value: 12
+  },
+  {
+    name: "Berlin",
+    value: 982
+  },
+  {
+    name: "Brandenburg",
+    value: 101
+  },
+  {
+    name: "Bremen",
+    value: 343
+  },
+  {
+    name: "Hamburg",
+    value: 385
+  },
+  {
+    name: "Darmstadt",
+    value: 5
+  },
+  {
+    name: "Gießen",
+    value: 42
+  },
+  {
+    name: "Kassel",
+    value: 20
+  },
+  {
+    name: "Mecklenburg-Vorpommern",
+    value: 5
+  },
+  {
+    name: "Braunschweig",
+    value: 3
+  },
+  {
+    name: "Hannover",
+    value: 520
+  },
+  {
+    name: "Luneburg",
+    value: 12
+  },
+  {
+    name: "Weser-Ems",
+    value: 380
+  },
+  {
+    name: "Arnsberg",
+    value: 20
+  },
+  {
+    name: "Detmold",
+    value: 28
+  },
+  {
+    name: "Düsseldorf",
+    value: 22
+  },
+  {
+    name: "Köln",
+    value: 62
+  },
+  {
+    name: "Munster",
+    value: 13
+  },
+  {
+    name: "Koblenz",
+    value: 32
+  },
+  {
+    name: "Rheinhessen-Pfalz",
+    value: 41
+  },
+  {
+    name: "Trier",
+    value: 56
+  },
+  {
+    name: "Saarland",
+    value: 85
+  },
+  {
+    name: "Chemnitz",
+    value: 52
+  },
+  {
+    name: "Dresden",
+    value: 20
+  },
+  {
+    name: "Leipzig",
+    value: 325
+  },
+  {
+    name: "Dessau",
+    value: 64
+  },
+  {
+    name: "Halle",
+    value: 10
+  },
+  {
+    name: "Magdeburg",
+    value: 2
+  },
+  {
+    name: "Schleswig-Holstein",
+    value: 5
+  },
+  {
+    name: "Thüringen",
+    value: 66
+  }
+];
+
+export const stats25032020: ResultEntry[] = [
+  {
+    name: "Niederbayern",
+    value: 474
+  },
+  {
+    name: "Oberbayern",
+    value: 3219
+  },
+  {
+    name: "Oberfranken",
+    value: 307
+  },
+  {
+    name: "Oberpfalz",
+    value: 465
+  },
+  {
+    name: "Schwaben",
+    value: 600
+  },
+  {
+    name: "Unterfranken",
+    value: 2699
+  },
+  {
+    name: "Freiburg",
+    value: 227
+  },
+  {
+    name: "Karlsruhe",
+    value: 172
+  },
+  {
+    name: "Stuttgart",
+    value: 451
+  },
+  {
+    name: "Tübingen",
+    value: 161
+  },
+  {
+    name: "Mittelfranken",
+    value: 159
+  },
+  {
+    name: "Berlin",
+    value: 1428
+  },
+  {
+    name: "Brandenburg",
+    value: 443
+  },
+  {
+    name: "Bremen",
+    value: 200
+  },
+  {
+    name: "Hamburg",
+    value: 1262
+  },
+  {
+    name: "Darmstadt",
+    value: 27
+  },
+  {
+    name: "Gießen",
+    value: 70
+  },
+  {
+    name: "Kassel",
+    value: 80
+  },
+  {
+    name: "Mecklenburg-Vorpommern",
+    value: 219
+  },
+  {
+    name: "Braunschweig",
+    value: 57
+  },
+  {
+    name: "Hannover",
+    value: 379
+  },
+  {
+    name: "Luneburg",
+    value: 60
+  },
+  {
+    name: "Weser-Ems",
+    value: 18
+  },
+  {
+    name: "Arnsberg",
+    value: 149
+  },
+  {
+    name: "Detmold",
+    value: 900
+  },
+  {
+    name: "Düsseldorf",
+    value: 280
+  },
+  {
+    name: "Köln",
+    value: 800
+  },
+  {
+    name: "Munster",
+    value: 300
+  },
+  {
+    name: "Koblenz",
+    value: 67
+  },
+  {
+    name: "Rheinhessen-Pfalz",
+    value: 55
+  },
+  {
+    name: "Trier",
+    value: 11
+  },
+  {
+    name: "Saarland",
+    value: 433
+  },
+  {
+    name: "Chemnitz",
+    value: 58
+  },
+  {
+    name: "Dresden",
+    value: 169
+  },
+  {
+    name: "Leipzig",
+    value: 200
+  },
+  {
+    name: "Dessau",
+    value: 14
+  },
+  {
+    name: "Halle",
+    value: 82
+  },
+  {
+    name: "Magdeburg",
+    value: 56
+  },
+  {
+    name: "Schleswig-Holstein",
+    value: 577
+  },
+  {
+    name: "Thüringen",
+    value: 437
+  }
+];
+
